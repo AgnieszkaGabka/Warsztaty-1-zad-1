510d596df418307f1db24587456f5f6bd3b75850
AgnieszkaGabka <94054240+AgnieszkaGabka@users.noreply.github.com>
2021-12-17 14:24:32 +0100

    Add files via upload
    changed laguage to english, according to task descritpion, added documentation

--- Gra w zgadywanie liczb.py
+++ Gra w zgadywanie liczb.py
@@ -2,16 +2,17 @@
 # próbie dostaje odpowiedź, czy podana liczba jest za mała, czy za duża.
 import random
 
-dobrze = False
-x = random.randint(1, 11)
+good = False
+x = random.randint(1, 11) #losowanie liczby miedzy 1 a 10
 
 
-while not dobrze: #dopóki gracz nie zgadnie liczby komputera, wykonaj poniższe działania
+while not good: #dopóki gracz nie zgadnie liczby komputera, wykonaj poniższe działania
     try:
         y = int(input("Guess the number 1 - 10: ")) #poproś o wpisanie zgadywanej liczby
         if x == y:
             print("You won!") #jeśli się zgadza, zwróć odpowiedź
             dobrze = True
+            break
         elif x > y:
             print("Too small!") # jeśli za mała, zwróć odpowiedż
         else:
@@ -19,4 +20,4 @@ while not dobrze: #dopóki gracz nie zgadnie liczby komputera, wykonaj poniższe
     except (TypeError, ValueError):
         print("Its not a number!") #jeśli gracz wpisze błędne dane, zwróć odpowiedź
 
-print(gra())
+
